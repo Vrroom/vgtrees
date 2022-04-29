8a453813b0fe95a530c7bf205273acea3d9e8d53
Sumit Chaturvedi <vrroom@github.com>
2022-04-29 10:34:46 +0000

    Ejected CRA
    I needed to create an entry point for visualizing the
    data that I'm going to get annotated. The easiest way
    to do that was to handle the build config myself.

---
+++ client/src/vis/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+
+
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/style.css'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
