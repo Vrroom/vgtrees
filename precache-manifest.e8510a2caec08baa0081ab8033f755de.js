f7ac8790d0c21a87b8e5aab9fc090cc015480164
Sumit Chaturvedi <sumit.chaturvedi@gmail.com>
2024-03-14 21:50:49 +0000

    Updates

---
+++ precache-manifest.e8510a2caec08baa0081ab8033f755de.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054d16ae4a09d75da9bd994800860b20",
+    "url": "/segment-anything-comic-ui/index.html"
+  },
+  {
+    "revision": "183354e73b53ebaddcc8",
+    "url": "/segment-anything-comic-ui/static/css/0.98036eff.chunk.css"
+  },
+  {
+    "revision": "10bdd544103a248b9b3a",
+    "url": "/segment-anything-comic-ui/static/css/index.5ff92c5c.chunk.css"
+  },
+  {
+    "revision": "576c39cd704086d885ef",
+    "url": "/segment-anything-comic-ui/static/css/vis.5ff92c5c.chunk.css"
+  },
+  {
+    "revision": "183354e73b53ebaddcc8",
+    "url": "/segment-anything-comic-ui/static/js/0.bfc12587.chunk.js"
+  },
+  {
+    "revision": "9dcc113e172332c53207033e6e5264bd",
+    "url": "/segment-anything-comic-ui/static/js/0.bfc12587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3950d5c26b6c3b07d848",
+    "url": "/segment-anything-comic-ui/static/js/5.a105782a.chunk.js"
+  },
+  {
+    "revision": "10bdd544103a248b9b3a",
+    "url": "/segment-anything-comic-ui/static/js/index.abb9dc33.chunk.js"
+  },
+  {
+    "revision": "820d4407cd8dd2bafd36",
+    "url": "/segment-anything-comic-ui/static/js/runtime-index.cb594e2a.js"
+  },
+  {
+    "revision": "317f175aa8eda93c0e47",
+    "url": "/segment-anything-comic-ui/static/js/runtime-vis.758b041c.js"
+  },
+  {
+    "revision": "576c39cd704086d885ef",
+    "url": "/segment-anything-comic-ui/static/js/vis.3a964fb1.chunk.js"
+  },
+  {
+    "revision": "b4cd52573d1484c16d23ff3a995d7787",
+    "url": "/segment-anything-comic-ui/static/media/contact.b4cd5257.svg"
+  },
+  {
+    "revision": "40cbd066726c8614215784f46b28b414",
+    "url": "/segment-anything-comic-ui/static/media/help.40cbd066.svg"
+  },
+  {
+    "revision": "3874cae0f6de8201c717bd51808524be",
+    "url": "/segment-anything-comic-ui/static/media/scissors.3874cae0.svg"
+  },
+  {
+    "revision": "0f365d59504945aedda9ea43980dfd8f",
+    "url": "/segment-anything-comic-ui/vis.html"
+  }
+]);
